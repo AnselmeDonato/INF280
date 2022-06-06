d49f382098dbe7e985ca5ec16dd88f6ac3a26482
anselme donato <anselme.donato2@gmail.com>
2022-06-06 23:57:51 +0200

    Essai de Divide & Conquer mais ça ne marche pas vraiment ...

--- Counting_substhreengs.cpp
+++ Counting_substhreengs.cpp
@@ -4,6 +4,48 @@
 
 using namespace std;
 
+/*
+DAC(a, i, j)
+{
+    if(small(a, i, j))
+      return(Solution(a, i, j))
+    else
+      m = divide(a, i, j)               // f1(n)
+      b = DAC(a, i, mid)                 // T(n/2)
+      c = DAC(a, mid+1, j)            // T(n/2)
+      d = combine(b, c)                 // f2(n)
+   return(d)
+}
+*/
+
+int DCA(string _strNumber, int* _count){
+  if(_strNumber.size() == 1){
+    int n = stoi(_strNumber);
+    cout << "minimal length : "<< stoi(_strNumber) << endl;
+    if(n % 3 == 0){
+      cout << " +1" << endl;
+      _count += 1;
+    }
+    return stoi(_strNumber);
+  }
+  else {
+    int mid = (int) _strNumber.size()/2;
+    cout << "mid : "<< mid << endl;
+    string half1(_strNumber.begin(), _strNumber.begin() + mid);
+    string half2(_strNumber.begin() + mid, _strNumber.end());
+    cout << "Half1 : " << half1 << " Half 2: "<< half2 << endl;
+    int a1 = DCA(half1, _count);
+    int a2 = DCA(half2, _count);
+    int d = a1 + a2;
+    cout << "d: "<< d << endl;
+    if(d % 3 == 0){
+      _count += 1;
+      cout << " comb +1 : " << _count << endl;
+    }
+    return d;
+  }
+}
+
 int main() {
   char c;
   string numberString = "";
@@ -18,15 +60,8 @@ int main() {
 
     else{
       if(startNumber){
-        for(int i = 0; i < numberString.size(); i ++){
-          long maybe_sub = 0;
-          for(int j = i; j < numberString.size(); j ++){
-            maybe_sub += numberString[j] - '0';
-            if( maybe_sub % 3 == 0){
-              nb_sub += 1;
-            }
-          }
-        }
+        cout << "Number string : "<< numberString << endl;
+        DCA(numberString, &nb_sub);
       }
 
       if(c == 10){
