14e93d8baa0524aabab66e9203433395416aeb92
Anselme DONATO <anselme.donato@telecom-paris.fr>
2022-06-06 19:02:59 +0200

    Counting substhreengs mauvais

---
+++ Counting_substhreengs.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  char c;
+  string numberString = "";
+  bool startNumber = false;
+  int nb_sub = 0;
+
+  while(scanf("%c", &c) != EOF) {
+    if(48 <= c && c <= 57) {
+      numberString.append(&c);
+      startNumber = true;
+    }
+
+    else{
+      if(startNumber){
+        for(int i = 0; i < numberString.size(); i ++){
+          string sub = "";
+          for(int j = i; j < numberString.size(); j ++){
+            sub.push_back(numberString[j]);
+            int number = stoi(sub);
+            if(number % 3 == 0){
+              nb_sub += 1;
+            }
+          }
+        }
+      }
+
+      if(c == 10){
+        cout << nb_sub << endl;
+        nb_sub = 0;
+      }
+
+      startNumber = false;
+      numberString.clear();
+
+    }
+
+  }
+
+  return 0;
+}
